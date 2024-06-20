ead6314a33b8ac339a29fc2acc80271320a4a708
Lixing Wu <lxw20030915@gmail.com>
2024-06-20 15:20:50 +0800

    finished writing the recipe writer llm but hv yet to test

--- mq_data1.py
+++ mq_data1.py
@@ -1,9 +1,10 @@
-import pika # for connecting & interacting w RabbitMQ (server chat client functions)
-import json # for parsing and creating JSON data
-import threading # for threading (concurrent processing DUH)
-import os # for OS interface for env variables
-import dotenv # for loading env variables from the .env file
-from queue import Queue # for using queue DS for message passing btw threads
+# for connecting & interacting w RabbitMQ (server chat client functions)
+import pika
+import json  # for parsing and creating JSON data
+import threading  # for threading (concurrent processing DUH)
+import os  # for OS interface for env variables
+import dotenv  # for loading env variables from the .env file
+from queue import Queue  # for using queue DS for message passing btw threads
 
 # the following imports are from .py files that define methods and classes for doing these functions
 # pretty self explanatory names
@@ -16,40 +17,56 @@ from image_process.logo_color_selection import getLogoColor1
 from image_process.title_logo_location import getTitleLogoPosition
 from color.colorAgent import getTitlecolor1
 
-dotenv.load_dotenv('.env', override=True) # this loads the env variables from .env
+# this loads the env variables from .env
+dotenv.load_dotenv('.env', override=True)
 # override=True means that if there r variables w the same names in both the .env file
 # and also the base env that we r in, the .env file vars will override the ones in base env
 
 # these r retrieving and setting env variables
-rabbitmq_host = os.getenv('rabbitmq_host') # gets hostname or IP
-rabbitmq_port = int(os.getenv('rabbitmq_port')) # gets port name that RabbitMQ is listening to
-rabbitmq_username = os.getenv('rabbitmq_username') # gets username for RabbitMQ authentification
-rabbitmq_password = os.getenv('rabbitmq_password') # gets password for ^
-virtual_host = os.getenv('virtual_host') # gets the virtual host within RabbitMQ that the client will connect to
-log_file_path = os.getenv('log_file_path') # the file path where log files will be written
-print("rabbitmq_host", rabbitmq_host, "rabbitmq_port", rabbitmq_port, "rabbitmq_username", rabbitmq_username, "rabbitmq_password", rabbitmq_password, "virtual_host", virtual_host, "log_file_path", log_file_path)
+rabbitmq_host = os.getenv('rabbitmq_host')  # gets hostname or IP
+# gets port name that RabbitMQ is listening to
+rabbitmq_port = int(os.getenv('rabbitmq_port'))
+# gets username for RabbitMQ authentification
+rabbitmq_username = os.getenv('rabbitmq_username')
+rabbitmq_password = os.getenv('rabbitmq_password')  # gets password for ^
+# gets the virtual host within RabbitMQ that the client will connect to
+virtual_host = os.getenv('virtual_host')
+# the file path where log files will be written
+log_file_path = os.getenv('log_file_path')
+print("rabbitmq_host", rabbitmq_host, "rabbitmq_port", rabbitmq_port, "rabbitmq_username", rabbitmq_username,
+      "rabbitmq_password", rabbitmq_password, "virtual_host", virtual_host, "log_file_path", log_file_path)
 # ^ prints out the values of all the retrieved environment variables
 
 # queue and routing key info
-queue_name = 'que_platform_ui_editor_algorithm' # the name of the queue within the RabbitMQ server where messages will be sent for processing
-routing_key = 'ui_editor_algorithm' # this is used in message routing to determine which queues should receive the message
-defaultmq_sent_exchange_name = 'que_platform_ui_editor_algorithm_result' # this s responsible for receiving messages from producers and routing them to queues
-defaultmq_sent_routing_key = 'ui_editor_algorithm_result' # this is the routing key used when sending messages to the exchange
+# the name of the queue within the RabbitMQ server where messages will be sent for processing
+queue_name = 'que_platform_ui_editor_algorithm'
+# this is used in message routing to determine which queues should receive the message
+routing_key = 'ui_editor_algorithm'
+# this s responsible for receiving messages from producers and routing them to queues
+defaultmq_sent_exchange_name = 'que_platform_ui_editor_algorithm_result'
+# this is the routing key used when sending messages to the exchange
+defaultmq_sent_routing_key = 'ui_editor_algorithm_result'
 
-message_queue = Queue() # this initializes the one queue we finna use that passes msgs btw threads
+# this initializes the one queue we finna use that passes msgs btw threads
+message_queue = Queue()
 
-def mqListen(): # this method listens to Rabbit MQ and processes received msgs
-    
-    global channel, connection # we declare these global cuz they hv to be used by multiple methods
+
+def mqListen():  # this method listens to Rabbit MQ and processes received msgs
+
+    # we declare these global cuz they hv to be used by multiple methods
+    global channel, connection
     # we first hv to make the connections to the server
-    connection = pika.BlockingConnection(pika.ConnectionParameters( # this makes connection
-        host=rabbitmq_host, port=rabbitmq_port, virtual_host=virtual_host, # using pika library
-        credentials=pika.PlainCredentials(rabbitmq_username, rabbitmq_password))) # thru the params we got
-    channel = connection.channel() # we use the est. connection to make a channel object
+    connection = pika.BlockingConnection(pika.ConnectionParameters(  # this makes connection
+        host=rabbitmq_host, port=rabbitmq_port, virtual_host=virtual_host,  # using pika library
+        credentials=pika.PlainCredentials(rabbitmq_username, rabbitmq_password)))  # thru the params we got
+    # we use the est. connection to make a channel object
+    channel = connection.channel()
     # ^ channel objects r used to talk w server in RabbitMQ
-    channel.queue_declare(queue=queue_name, durable=True) # we make a queue on the channel called queue_name
+    # we make a queue on the channel called queue_name
+    channel.queue_declare(queue=queue_name, durable=True)
     # setting durable=True means that even if the channel restarts, the queue servives
-    channel.basic_qos(prefetch_count=1) # this means that the queue that we just made which is acting
+    # this means that the queue that we just made which is acting
+    channel.basic_qos(prefetch_count=1)
     # as the collection for the client connection we hv will only fetch 1 new message at a time
     # if we had multiple connections and there was no prefetch_count then one connection might take in
     # a lot of messages at once while the others didn't take any so this ensures that each connection
@@ -61,43 +78,52 @@ def mqListen(): # this method listens to Rabbit MQ and processes received msgs
     # so that means that having multiple workers ISN'T useless if we hv them doing diff tasks for the same msg
     # but if we set prefetch_count to 2 maybeeee that would be more effective?
 
-
     # the following is a callback method for processing msgs
     # callback means that it will be passed into another func as an arg for event-driven programming
     # event-driven means like servers and shit
-    
-    def callback(ch, method, properties, body): # the params are as follows (altho properties isn't used)
+
+    # the params are as follows (altho properties isn't used)
+    def callback(ch, method, properties, body):
         # @ch the channel communication object
         # @method info abt msg delivery
         # @properties the properties of the msg
         # @body the actual msg
-        
-        if body: # so if there's actually a body to the msg
+
+        if body:  # so if there's actually a body to the msg
             try:
-                data = json.loads(body) # we convert the JSON string msg to Python 'data'
-                print("+++++++++++++++++++++++++++++++++++++++++") # this is for debugging like as in
+                # we convert the JSON string msg to Python 'data'
+                data = json.loads(body)
+                # this is for debugging like as in
+                print("+++++++++++++++++++++++++++++++++++++++++")
                 # ^ did we acc get to this point if so then we print this
-                message_queue.put(data) # we put the msg we parsed into the queue we made earlier
-                ch.basic_ack(delivery_tag=method.delivery_tag) # this part is NECESSARY
+                # we put the msg we parsed into the queue we made earlier
+                message_queue.put(data)
+                # this part is NECESSARY
+                ch.basic_ack(delivery_tag=method.delivery_tag)
                 # we r telling RabbitMQ that we acc received the msg
                 # so RabbitMQ won't send the msg to anyone else thinking this one didn't receive it
                 # now RabbitMQ can remove this msg from their "to-send" queue -is-> queue_name in our code
                 # basic_ack - pika method for acknowledging
-                
-            except Exception as e: # basically if we didn't receive anything (DUH)
+
+            # basically if we didn't receive anything (DUH)
+            except Exception as e:
                 print(f"error: {e}")
 
     # OK now that we hv the callback method defined this following step is important too
     # we r setting up a consumer to consume msgs from queue_name
     # we use the callback method we j defined to say *this process* is how ur gonna consume msgs from queue_name
     # we hv auto_ack=False bc in our method we hv set up our own acknowledgement just for security
-    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=False)
-    print("Starting to consume from RabbitMQ.") # this means that we hv started to consume reliably
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=False)
+    # this means that we hv started to consume reliably
+    print("Starting to consume from RabbitMQ.")
     # ^ this basically shows us like oh everything up til setting up the connection is all g
     # now we just need to start receiving the msgs (which we do using callback)
-    channel.start_consuming() # so let's start
+    channel.start_consuming()  # so let's start
 
-def process_message(data): # there is acc no explanation necessary this is like a convoluted switch-case
+
+# there is acc no explanation necessary this is like a convoluted switch-case
+def process_message(data):
     # for diff msg types that we receive we do diff tasks like yk this
     try:
         print("Processing message:", data)
@@ -133,10 +159,12 @@ def process_message(data): # there is acc no explanation necessary this is like 
     except Exception as e:
         print(f"error: {e}")
 
-def worker(): # these workers do the threading part and they r the ones who consume msgs
-    while 1: # this is an infinite loop cuz we never really wanna stop
+
+def worker():  # these workers do the threading part and they r the ones who consume msgs
+    while 1:  # this is an infinite loop cuz we never really wanna stop
         try:
-            message = message_queue.get(timeout=1) # so RabbitMQ sent us msgs from queue_name
+            # so RabbitMQ sent us msgs from queue_name
+            message = message_queue.get(timeout=1)
             # ^ that we then put into message_queue and now the workers r getting them from
             # ^ message_queue so that they can acc process them
             # note that timeout=1 is set bc that's the max amt of time the get() method waits
@@ -144,48 +172,55 @@ def worker(): # these workers do the threading part and they r the ones who cons
             # which, why is that an issue? prolly bc that blocks other workers
             # this means that we can allow workers to periodically check before moving on
             # ALSO KNOW THAT get() removes the task from the queue
-            
-            process_message(message) # we r calling the method to figure out what the msg entails
+
+            # we r calling the method to figure out what the msg entails
+            process_message(message)
             # and then obvi in this method we acc do the stuff the msg wants us to do asw
-            
-            message_queue.task_done() # we basically say to the message_queue that we j did this task
+
+            # we basically say to the message_queue that we j did this task
+            message_queue.task_done()
             # SO YOU MAY NOT REALIZE BUT these type of queues have an internal counter for tasks
             # and like ya, get() removes the task from the queue but the internal counter of tasks
             # is not decremented yet... BUT NOW IT IS by marking the task as done
-            
-        except Exception as e: # this basically is like if there's an exception j move on
+
+        except Exception as e:  # this basically is like if there's an exception j move on
             continue
 
-def listen_data(): # this part acc initiates the threads by using the workers
 
-    threads = [] # this is an empty list of threads
-    num_worker_threads = 3 # we set the max num of workers to 3 so 3 can work concurrently at once
-    
+def listen_data():  # this part acc initiates the threads by using the workers
+
+    threads = []  # this is an empty list of threads
+    # we set the max num of workers to 3 so 3 can work concurrently at once
+    num_worker_threads = 3
+
     # this loop j like populates the empty list w the workers
     for _ in range(num_worker_threads):
-        thread = threading.Thread(target=worker) # this makes a new thread
+        thread = threading.Thread(target=worker)  # this makes a new thread
         # ^ the target=worker is specifying which function actually executes the thread - in this case, worker
-        thread.start() # we start the thread
-        threads.append(thread) # we add the thread we j made to the list
+        thread.start()  # we start the thread
+        threads.append(thread)  # we add the thread we j made to the list
 
-    listener_thread = threading.Thread(target=mqListen) # now we make a single thread for acc listening to RabbitMQ
+    # now we make a single thread for acc listening to RabbitMQ
+    listener_thread = threading.Thread(target=mqListen)
     # ^ this uses the mqListen method we made in the first half to do the thread execution
-    listener_thread.start() # we start the thread
-    
+    listener_thread.start()  # we start the thread
+
     # OK to preface the next two calls, this is the function of join():
     # join is used to wait for a thread to complete its execution before proceeding w the rest of the code
     # when join() is called on a thread, the thread that calls it will pause its execution
     # ^ until the thread that is called is finished executing
     # so like if, in main_thread we call sub_thread.join(), then main_thread will pause until sub_thread is done
-    
+
     # so below, this means that our main thread (the main program) will:
-    listener_thread.join() # not end until the listener thread completes its task to consume msgs from RabbitMQ
-    message_queue.join() # and wait for message_queue to be empty before stopping
-    
+    # not end until the listener thread completes its task to consume msgs from RabbitMQ
+    listener_thread.join()
+    message_queue.join()  # and wait for message_queue to be empty before stopping
+
     # ^ so the above basically says until the msgs are done being consumed from RabbitMQ AND
     # until the workers hv finished processing all msgs, the main thread will not terminate
     # I mean obviously like u can terminate the program manually but this is moreso for:
     # synchronizing thread terminization and stopping pre-mature exits (BOO data leaks)
 
-if __name__ == "__main__": # this means that this is only ran when it itself is ran directly, not thru any imports or shit
-    listen_data() # now we call this last method to be ran
+
+if __name__ == "__main__":  # this means that this is only ran when it itself is ran directly, not thru any imports or shit
+    listen_data()  # now we call this last method to be ran
---
+++ recipe-write.py
@@ -0,0 +1,268 @@
+from langchain.prompts import PromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field  # for grading
+from langchain_community.chat_models import QianfanChatEndpoint
+from langchain.embeddings import QianfanEmbeddingsEndpoint
+from langchain_core.output_parsers import StrOutputParser
+import numpy as np
+from product_analysis import split_text, rag_search
+
+# key
+qianfan_ak = "DAEEqjuvglLTgQMCXqRvqfUj"
+qianfan_sk = "s0AJ849GNB6440lwLWDvGuNEJNrgrbQ3"
+
+# models
+llm = QianfanChatEndpoint(model="ERNIE-4.0-8K", streaming=True,
+                          qianfan_ak=qianfan_ak, qianfan_sk=qianfan_sk, penalty_score=1)
+embed = QianfanEmbeddingsEndpoint(
+    model="bge_large_zh", endpoint="bge_large_zh", qianfan_ak=qianfan_ak, qianfan_sk=qianfan_sk)
+
+
+def make_prompts():
+
+    prompts = {}
+
+    prompt1 = PromptTemplate(
+
+        template="""
+        
+        请考虑{prod}的特点、市场定位和用户反馈，
+        详细说明该产品在市场上的独特卖点和竞争优势。
+        请提供关于该产品如何吸引目标用户群体的见解。\n\n
+        
+        主要使用以下信息来得出答案: \n
+        
+        {context}
+        
+        """,
+
+        input_variables=["prod", "context"]
+
+    )
+
+    prompt2 = PromptTemplate(
+
+        template="""
+        
+        请分析使用{prod}的主要目标人群的特征、偏好和需求。
+        结合季节和节日，说明这些人群在这个时间点对{prod}的需求和期望。\n\n
+        
+        主要使用以下信息来得出答案: \n
+        
+        {context}
+        
+        """,
+
+        input_variables=["prod", "context"]
+
+    )
+
+    prompt3 = PromptTemplate(
+
+        template="""
+        
+        请详细说明{prod}在{season}时间点的最佳使用方法和搭配。
+        考虑到食材的季节性和营养价值，
+        解释为什么在这个时间点使用{prod}最为合适。\n\n
+        
+        主要使用以下信息来得出答案: \n
+        
+        {context}
+        
+        """,
+
+        input_variables=["prod", "season", "context"]
+
+    )
+
+    prompt4 = PromptTemplate(
+
+        template="""
+        
+        请提供关于在{season}时间点使用{prod}进行促销活动或节日活动的建议。
+        详细说明如何利用节日气氛和季节特点来提升{prod}的销量和品牌知名度。\n\n
+        
+        主要使用以下信息来得出答案: \n
+        
+        {context}
+        
+        """,
+
+        input_variables=["prod", "season", "context"]
+
+    )
+
+    prompts[prompt1] = "产品特点 市场定位 用户反馈 卖点 竞争优势"
+    prompts[prompt2] = "目标人群 特征 偏好 需求 季节 节日"
+    prompts[prompt3] = "最佳使用方法 搭配 食材季节性 营养价值"
+    prompts[prompt4] = "促销活动 节日活动 提升销量 品牌知名度"
+
+    return prompts
+
+
+class GradeDocuments(BaseModel):
+    """搜索结果相关性检查的二进制评分。"""
+
+    binary_score: str = Field(
+        description="搜索结果与搜索相关，'yes' 或 'no'"
+    )
+
+
+def process_search(query):
+
+    q_embed = embed.embed_query(query)
+
+    search_result = rag_search(query)
+
+    all_chunks = []  # makes a list to store the chunks
+    for result in search_result:  # for every single result we get
+        chunks = split_text(result)
+        all_chunks.extend(chunks)
+
+    search_embed = []  # makes a list for the embeddings bc the embedding model has a max num of tokens that is exceeded by big chunks
+    i1, i2 = 0, 0
+
+    while i2 < len(all_chunks) - 1:
+        i2 += 1
+        search_embed.extend(embed.embed_documents(all_chunks[i1:i2]))
+        i1 += 1
+
+    search_embed = np.array(search_embed)
+
+    # this will get all the dot products for our searches X context
+    similarity_scores = np.dot(q_embed, search_embed.T)
+    filtered_results = [(result, score) for result, score in zip(
+        search_result, similarity_scores) if score > 0.5]
+
+    max_ctxt = 3
+    if len(filtered_results) < 3:
+        max_ctxt = len(filtered_results)
+    top_results = sorted(filtered_results, key=lambda x: x[1], reverse=True)[
+        :max_ctxt]
+
+    rag_results = " ".join([result[0] for result in top_results])
+
+    return rag_results
+
+
+def re_search(query):
+
+    promptV2 = PromptTemplate(
+
+        template="""    
+        你是一个搜索输入重写器，将搜索输入转换为优化后的版本以便进行网络搜索。\n     
+        优化以下内容：\n    
+        {query}     
+        """,
+        input_variables=["query"]
+
+    )
+
+    rewriter = promptV2 | llm | StrOutputParser()
+
+    new_query = rewriter.invoke({"query": query})
+
+    return process_search(new_query)
+
+
+def crag_search(prod_des, season):
+
+    prompts = make_prompts()
+    context, queries, search_results = [], [], []
+
+    for prompt in prompts.keys():
+        query = prod_des + prompts[prompt]
+        queries.append(query)
+        text = process_search(query)
+        search_results.append(text)
+
+    structured_llm_grader = llm.with_structured_output(GradeDocuments)
+
+    grade_prompt = PromptTemplate(
+        template="""       
+        搜索结果: \n\n {result} 
+        \n\n 搜索输入: {query}
+        """,
+        input_variables=["query", "result"]
+    )
+
+    retrieval_grader = grade_prompt | structured_llm_grader
+
+    grades = [True, True, True, True]
+
+    for i, query in enumerate(queries):
+        result = retrieval_grader.invoke(
+            {"query": query, "result": search_results[i]})
+        bs = result.binary_score
+
+        if bs == "no":
+            grades[i] = False
+
+    if not grades[0] or not grades[1] or not grades[2] or not grades[3]:
+
+        if not grades[0]:
+            query = prod_des + prompts[0][1]
+            text = re_search(query)
+            search_results[0] = text
+
+        if not grades[1]:
+            query = prod_des + prompts[1][1]
+            text = re_search(query)
+            search_results[1] = text
+
+        if not grades[2]:
+            query = prod_des + prompts[2][1]
+            text = re_search(query)
+            search_results[2] = text
+
+        if not grades[3]:
+            query = prod_des + prompts[3][1]
+            text = re_search(query)
+            search_results[3] = text
+
+    for i, prompt in enumerate(prompts):
+        expertGPT = prompt[0] | llm | StrOutputParser()
+
+        ans = expertGPT.invoke(
+            {"prod": prod_des, "season": season, "context": search_results[i]})
+
+        context.append(ans)
+
+    return context
+
+
+def call_reciGPT():
+
+    prod_des = input("Enter product:")
+    season = input("Enter season:")
+
+    context = crag_search(prod_des, season)
+
+    prompt5 = PromptTemplate(
+        template="""你是一个美食大师、厨艺大师，你可以对指定的食品或食材，
+        创作出当季的菜品推荐，你熟悉食材的生长规律、生长习性、成分变化，你极度推荐每个食材在一年中最佳食用的月份、
+        季节，你用你的专业性和优美清新的文字功底，向人们解释为什么这个月食用最佳；
+        你与众不同、独到的专业见解和知识面，让读者耳目一新，能提供目标客户人群的情绪价值。\n\n
+        请考虑以下专家提供的建议。\n
+        产品运营专家说：{context1}。\n
+        人群运营专家说：{context2}。\n
+        时令食材运营专家说：{context3}。\n
+        节点活动运营专家说：{context4}。\n\n
+        针对给定的{prod}（商品）和{season}时间点，写三道推荐菜品:""",
+        input_variables=["prod", "context1", "context2",
+                         "context3", "context4", "season"]
+    )
+
+    reciGPT = prompt5 | llm | StrOutputParser()
+
+    try:
+        ans = reciGPT.invoke(
+            {"prod": prod_des, "context1": context[0], "context2": context[1],
+             "context3": context[2], "context4": context[3], "season": season})
+    except Exception as e:
+        print("error: ", e)
+        return
+
+    return ans
+
+
+call_reciGPT()
